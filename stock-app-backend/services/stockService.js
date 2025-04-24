6e1c4ecd05222730bef2faa3ab5927a6dae8394e
Piyush <piyushverma.11rpvv@gmail.com>
2025-04-25 00:35:01 +0530

    removed: Extra lines

--- stock-app-backend/services/stockService.js
+++ stock-app-backend/services/stockService.js
@@ -1,26 +1,3 @@
-// import axios from "axios";
-
-// const getStockPrice = async (symbol) => {
-//   const apiKey = process.env.FINNHUB_API_KEY;
-//     if (!apiKey) {
-//         console.error("API key is not defined");
-//         return null;
-//     }
-//   const url = `https://finnhub.io/api/v1/quote?symbol=${symbol}&token=${apiKey}`;
-//     console.log("Fetching stock price from URL:", url);
-//   try {
-//     const response = await axios.get(url);
-//     if (response.status !== 200) {
-//       throw new Error(`Error fetching stock data: ${response.status}`);
-//     }
-//     return response.data.c; // current price
-//   } catch (error) {
-//     console.error("Stock API error:", error);
-//     return null;
-//   }
-// };
-
-// export default getStockPrice;
 import axios from "axios";
 
 const fetchQuote = async (symbol) => {
@@ -33,7 +10,7 @@ const fetchQuote = async (symbol) => {
   const { data, status } = await axios.get(url);
   if (status !== 200) throw new Error("Failed to fetch data");
 
-  return data.c; // current price
+  return data.c; 
 };
 
 export default fetchQuote;
